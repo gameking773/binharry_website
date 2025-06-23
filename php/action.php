4e5d832107db1663922cffd6e8d1b68674781725
Khalil El kadiri <khalil.el-kadiri@etudiant.univ-reims>
2025-06-23 22:23:37 +0200

    Ajout de la récupération et envoi du mail, à tester

---
+++ php/action.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1); 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupération et nettoyage des données du formulaire
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Remplissage de l'entête du mail
+    $to = "bdebinharry@gmail.com"; 
+    $subject = "Nouveau formulaire";
+    $headers = "De: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Mail
+    $body = "You have received a new message:\n\n";
+    $body .= "Nom: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    // Envoi du mail
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Nous avons bien reçu votre message. Merci de nous avoir contactés, $name.";
+    } else {
+        echo "Oopsie, le message ne s'est pas envoyé. Essaie encore.";
+    }
+}git commit -m "Ajout de la récupération et envoi du mail, à tester"
