97fefb8492025c7732572d7dc962bcce180dc219
Rainbow51 <ruddy.gobillard@outlook.fr>
2025-07-01 17:32:52 +0200

    Fix: Indentation du HTML

--- public/about.php
+++ public/about.php
@@ -6,25 +6,24 @@ use Html\WebPage as WebPage;
 $WebPage = new WebPage("À propos");
 
 $WebPage -> appendContent("<h1 class='page_info'>CONTACT</h1>
-            <h2>L'équipe</h2>
-            <ul class='team-list'>
-                <li>Votre dévoué Président: Khalil El Kadiri</li>
-                <li>Votre légendaire Vice-Président & responsable évènementiel : Julien Baetens</li>
-                <li>Votre Trésorier: Raphaël Véry</li>
-                <li>Votre Secrétaire: Noa Re</li>
-                <li>Votre Designer d'affiches & CM: Ruddy Gobillard</li> 
-            </ul>
-            <h2>Nous contacter</h2>
-            <form action='../php/action.php' method='post'>
-                <label for='name'>Nom:</label>
-                <input type='text' id='name' name='name' required><br>
-                <label for='email'>Email:</label>           
-                <input type='email' id='email' name='email' required><br>
-                <label for='message'>Message:</label><br>   
-                <textarea id='message' name='message' rows='4' cols='50' required></textarea><br>
-                <input type='submit' value='Envoyer' class='submit-button'>       
-            </form>
-            <p class='note'>Notez que ce formulaire ne sera très certainement ni lu, ni pris en compte.</p>
-");
+                <h2>L'équipe</h2>
+                <ul class='team-list'>
+                    <li>Votre dévoué Président: Khalil El Kadiri</li>
+                    <li>Votre légendaire Vice-Président & responsable évènementiel : Julien Baetens</li>
+                    <li>Votre Trésorier: Raphaël Véry</li>
+                    <li>Votre Secrétaire: Noa Re</li>
+                    <li>Votre Designer d'affiches & CM: Ruddy Gobillard</li> 
+                </ul>
+                <h2>Nous contacter</h2>
+                <form action='../php/action.php' method='post'>
+                    <label for='name'>Nom:</label>
+                    <input type='text' id='name' name='name' required><br>
+                    <label for='email'>Email:</label>           
+                    <input type='email' id='email' name='email' required><br>
+                    <label for='message'>Message:</label><br>   
+                    <textarea id='message' name='message' rows='4' cols='50' required></textarea><br>
+                    <input type='submit' value='Envoyer' class='submit-button'>       
+                </form>
+                <p class='note'>Notez que ce formulaire ne sera très certainement ni lu, ni pris en compte.</p>");
 
 echo $WebPage -> toHTML();
--- public/events.php
+++ public/events.php
@@ -7,36 +7,35 @@ use Html\WebPage as WebPage;
 $WebPage = new WebPage("Événements");
 
 $WebPage -> appendContent("<h1 class='page_info'>LES ÉVÈNEMENTS</h1>
-            <div class='content-events'>
-                <div class='card-container'>
-                    <article class='card' id='Event1'>
-                        <div class='card-content'>
-                            <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                        </div>
-                    </article> 
-                </div>
-                <div class='card-container'>
-                    <article class='card' id='Event2'>
-                        <div class='card-content'>
-                            <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                        </div>
-                    </article> 
-                </div>
-                <div class='card-container'>
-                    <article class='card' id='Event3'>
-                        <div class='card-content'>
-                            <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                        </div>
-                    </article> 
-                </div>
-                <div class='card-container'>
-                    <article class='card' id='Event4'>
-                        <div class='card-content'>
-                            <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                        </div>
-                    </article> 
-                </div>
-            </div>
-");
+                <div class='content-events'>
+                    <div class='card-container'>
+                        <article class='card' id='Event1'>
+                            <div class='card-content'>
+                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
+                            </div>
+                        </article> 
+                    </div>
+                    <div class='card-container'>
+                        <article class='card' id='Event2'>
+                            <div class='card-content'>
+                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
+                            </div>
+                        </article> 
+                    </div>
+                    <div class='card-container'>
+                        <article class='card' id='Event3'>
+                            <div class='card-content'>
+                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
+                            </div>
+                        </article> 
+                    </div>
+                    <div class='card-container'>
+                        <article class='card' id='Event4'>
+                            <div class='card-content'>
+                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
+                            </div>
+                        </article> 
+                    </div>
+                </div>");
 
 echo $WebPage -> toHTML();
