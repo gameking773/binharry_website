40680c88bb52c5913619f8aa53d93e6e5c994877
Khalil El kadiri <khalil.el-kadiri@etudiant.univ-reims>
2025-07-22 23:13:58 +0200

    feat : ajout de l'easter egg Sandron

--- public/action.php
+++ public/action.php
@@ -39,7 +39,7 @@ else{
     $webpage = new WebPage("Résultat de la recherche : $search");
 
     if ($search == "sandron") {
-        $webpage->appendContent("<p>Sandron/</p>");
+        $webpage->appendContent("<img src='https://media.discordapp.net/attachments/1107765849235931207/1315988943942193202/f15872f7-7bca-4be2-8587-44c1cac738a9.gif?ex=6880b9b6&is=687f6836&hm=3ced9d24ef8d94f4d3f6870a3a421ded2e034413ac2b72dc50892a8c8095b55f&=&width=140&height=140'>");
     }
     else {
         $webpage->appendContent("<p>Nothing to see here, try again</p>");
