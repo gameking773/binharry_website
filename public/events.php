ed4d967f82141157e0d85d900df16e8676f956e7
Rainbow51 <ruddy.gobillard@outlook.fr>
2025-08-12 01:22:38 +0200

    Refactor: Suppression de l'ancien affichage des évènements

--- public/events.php
+++ public/events.php
@@ -6,38 +6,6 @@ use Entity\Collection\EventCollection as EventCollection;
 
 $WebPage = new WebPage("Événements");
 
-/** ancienne version
-$WebPage -> appendContent("<div class='content-events'>
-                    <div class='card-container'>
-                        <article class='card' id='Event1'>
-                            <div class='card-content'>
-                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                            </div>
-                        </article> 
-                    </div>
-                    <div class='card-container'>
-                        <article class='card' id='Event2'>
-                            <div class='card-content'>
-                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                            </div>
-                        </article> 
-                    </div>
-                    <div class='card-container'>
-                        <article class='card' id='Event3'>
-                            <div class='card-content'>
-                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                            </div>
-                        </article> 
-                    </div>
-                    <div class='card-container'>
-                        <article class='card' id='Event4'>
-                            <div class='card-content'>
-                                <p class='card-description'>  <br><a href='' class='Readmore'>Read More</a></p>
-                            </div>
-                        </article> 
-                    </div>");
-*/
-
 $WebPage -> appendContent("<h1 class='page_info'>LES ÉVÈNEMENTS</h1>
     <div class='card_list'>");
 
