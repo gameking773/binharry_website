b93784a49cd17c445ec31b4cd46be2b634916666
Khalil <elkadirikhalil2006@gmail.com>
2025-08-26 16:49:24 +0200

    ajout des getters et de findById dans Photo.php

--- src/Entity/Photo.php
+++ src/Entity/Photo.php
@@ -11,5 +11,45 @@ class Photo
     private int $id;
     private string $jpeg;
 
+    /**
+     * Get the value of afficheId
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
+    /**
+     * Get the value of jpeg
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+    /**
+     * Returns the instance of Photo which is identified by the id in parameter
+     *
+     * @param $id int id to search
+     * @return Photo that corresponds to the id
+     */
+    public static function findById(int $id): Photo
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT photoId, jpeg
+            FROM photo
+            WHERE photoId = ?
+            SQL
+        );
+
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Photo::class);
+        if (($photo = $stmt->fetch()) === false) {
+            throw new EntityNotFoundException();
+        }
+        return $photo;
+    }
 }
