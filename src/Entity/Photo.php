84e15299bd2e1994f27f59df1f0552f98ce3431b
Khalil <elkadirikhalil2006@gmail.com>
2025-08-26 16:32:42 +0200

    suppression des getters et setters

--- src/Entity/Photo.php
+++ src/Entity/Photo.php
@@ -11,45 +11,5 @@ class Photo
     private int $id;
     private string $jpeg;
 
-    /**
-     * Returns the ID of the Photo.
-     *
-     * @return int ID of the Photo.
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
 
-    /**
-     * Sets the ID of the Photo.
-     *
-     * @param int $id new ID of the Photo.
-     * @return void
-     */
-    private function setId(int $id): void
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * Returns the jpeg of the Photo.
-     *
-     * @return string jpeg of the Photo.
-     */
-    public function getJpeg(): string
-    {
-        return $this->jpeg;
-    }
-
-    /**
-     * Sets the jpeg of the Photo.
-     *
-     * @param string $jpeg new jpeg of the Photo.
-     * @return void
-     */
-    private function setJpeg(string $jpeg): void
-    {
-        $this->jpeg = $jpeg;
-    }
 }
